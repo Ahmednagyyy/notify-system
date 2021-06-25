8fefa38e46ed61a5cc2798f2eb42dac7b20b6191
Ahmed Mohamed Nagy <ahmed.nagii2511@gmail.com>
2021-06-25 03:49:51 +0200

    Add Spring application main class

---
+++ notification-gatway/src/main/java/com/notify/notificationgatway/NotificationGatwayApplication.java
@@ -0,0 +1,13 @@
+package com.notify.notificationgatway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationGatwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationGatwayApplication.class, args);
+	}
+
+}
